9cee44b23af9ab69a3399c4a9b4323c1c345b0e8
katyamichal <katyamichal7@gmail.com>
2024-05-29 15:43:27 +0300

    add available to several views

--- CarsApp/CarsApp/Modules/CarListModule/Cell/CarTableViewCell.swift
+++ CarsApp/CarsApp/Modules/CarListModule/Cell/CarTableViewCell.swift
@@ -23,6 +23,7 @@ final class CarTableViewCell: UITableViewCell {
         setupCell()
     }
     
+    @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
